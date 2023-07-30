729710f670f4cc8fa2d83f7a02d6b76f45540199
Dani <prihoczki.daniel@gmail.com>
2023-07-30 21:48:52 +0200

    Finished everything

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -1,5 +1,38 @@
-public class Main {
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Random randomNumber = new Random();
+        int findThisNumber = randomNumber.nextInt(100);
+        System.out.println(findThisNumber);
+
+
+        System.out.println("Guess a number between 0 and 100: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+
+
+
+        while(number < 0 || number > 100){
+            System.out.println("The number must be between 0 and 100!");
+            number = scanner.nextInt();
+        }
+
+        while (number < findThisNumber) {
+            System.out.println("The number is higher than your guess!");
+            number = scanner.nextInt();
+        }
+
+        while(number > findThisNumber){
+            System.out.println("The number is lower than your guess!");
+            number = scanner.nextInt();
+        }
+
+        if (number == findThisNumber) {
+            System.out.println("You guessed it!");
+        }
+
     }
 }
